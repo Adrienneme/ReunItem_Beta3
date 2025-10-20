ffcf37d9a0bf9693d0988fd1f2afe0f6e6de07ce
Troyden101 <cjusyt@gmail.com>
2025-10-20 19:26:01 +0800

    Added call Image typ and image upload

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -41,3 +41,14 @@ UserSchemas.LoginResponse.update_forward_refs()
 #Changes I added
 #Imageupload with caption for storing
 
+
+class ImageType(str, Enum):
+    lost = "lost"
+    found = "found"
+
+class ImageUpload(BaseModel):
+    first_name: str
+    last_name: str
+    image_url: str
+    image_type: ImageType 
+ 
